4ff2a190fac7c3af6ce481102e73daec538aed2c
Atul Rungta <atulrungta@guest-v44-00471.dhcp.unc.edu>
2014-11-09 20:38:01 -0500

    first commit

---
+++ getting_started/build/getting_started.build/Debug/cg.build/Objects-normal/i386/sampleGUI.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/atulrungta/Desktop/UNC_Stuff/COMP768/Assignment3/getting_started/sampleGUI.cpp \
+  /Users/atulrungta/Desktop/UNC_Stuff/COMP768/Assignment3/getting_started/sampleGUI.h \
+  /Users/atulrungta/Desktop/UNC_Stuff/COMP768/Assignment3/getting_started/texture.h \
+  /Users/atulrungta/Desktop/UNC_Stuff/COMP768/Assignment3/getting_started/Color.h \
+  /Users/atulrungta/Desktop/UNC_Stuff/COMP768/Assignment3/getting_started/GL/glui.h
---
+++ getting_started/build/getting_started.build/Debug/cg.build/Objects-normal/i386/texture.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/atulrungta/Desktop/UNC_Stuff/COMP768/Assignment1/getting_started/texture.cpp \
+  /Users/atulrungta/Desktop/UNC_Stuff/COMP768/Assignment1/getting_started/texture.h
